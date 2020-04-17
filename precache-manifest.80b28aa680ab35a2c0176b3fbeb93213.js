1297e39625a83456a38aac72b9e6bfbb475e7c3e
Drutten <rosi.drott@gmail.com>
2020-04-17 11:04:43 +0200

    Updates

---
+++ precache-manifest.80b28aa680ab35a2c0176b3fbeb93213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76486ee3f7df693c9b0fe4580ca91775",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e61a637dc4cd69cc8136",
+    "url": "/todolist/static/css/main.4bc32853.chunk.css"
+  },
+  {
+    "revision": "803dea5a63d3dc8b7efc",
+    "url": "/todolist/static/js/2.5e54b755.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todolist/static/js/2.5e54b755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61a637dc4cd69cc8136",
+    "url": "/todolist/static/js/main.7fd83791.chunk.js"
+  },
+  {
+    "revision": "9ee46fa7b8e64eba8138",
+    "url": "/todolist/static/js/runtime-main.0c5099da.js"
+  }
+]);
